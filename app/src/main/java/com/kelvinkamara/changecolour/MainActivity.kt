bf8d2709115c17fe05f32be15916b2d83fc98522
kkamara <kamaracomputers@gmail.com>
2026-01-13 17:16:57 +0000

    Change background colour to red on button click

--- app/src/main/java/com/kelvinkamara/changecolour/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/changecolour/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.kelvinkamara.changecolour
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var view: View
+    private lateinit var button: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.view)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        view = findViewById(R.id.view)
+        button = findViewById(R.id.button_change)
+
+        button.setOnClickListener {
+            view.setBackgroundColor(Color.RED)
         }
     }
 }
